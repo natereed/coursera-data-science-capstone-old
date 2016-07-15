f7c9c4c3b15a994b814c94789361e94b21e94bdb
natereed <nate@natereed.com>
2016-07-15 10:43:56 -0500

    Add script for downloading swiftkey data. Update predict.R

---
+++ predictive-model/text2vec_impl/download_data.R
@@ -0,0 +1,2 @@
+setwd("/home/nate/Coursera/Capstone")
+download.file("https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip", destfile="Coursera-SwiftKey.zip")
--- predictive-model/text2vec_impl/predict.R
+++ predictive-model/text2vec_impl/predict.R
@@ -12,19 +12,3 @@ vocab <- create_vocabulary(it, ngram=c(1L, 8L));
 
 vectorizer <- vocab_vectorizer(vocab)
 
-# Reinitialize iterator
-it <- itoken(blogs_data, 
-             preprocess_function = tolower, 
-             tokenizer = word_tokenizer);
-dtm <- create_dtm(it, vectorizer, type='dgTMatrix')
-
-# Term frequencies of n-grams that match "real_things"
-# Corresponds to all the documents in the corpus
-dtm[,c("real_things")]
-
-# Which term frequencies are greater than zero?
-which(dtm[,c("real_things")] > 0)
-
-# Get the term frequencies of all n-grams for documents that contain the n-gram "real_things"
-dtm[which(dtm[,c("real_things")] > 0),]
-
